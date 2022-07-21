0caaa6f3e1ce3446f4cea979cb7c6d7d9188575f
pawan-appzoy <pawan@appzoy.com>
2022-07-21 15:16:32 +0530

    code update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 exports.__esModule = true;
 exports.doSomeStuff = exports.sendMessage = void 0;
-var dummyClass_1 = require('./example/dummyClass');
+var dummyClass_1 = require("./example/dummyClass");
 // export let employee = new Employee('Sample message', 100, 'pawan');
 function sendMessage() {
-  var employee = new dummyClass_1.Employee('Sample message', 100, 'pawan');
-  return employee.sendMessage();
+    var employee = new dummyClass_1.Employee('Sample message', 100, 'pawan');
+    return employee.sendMessage();
 }
 exports.sendMessage = sendMessage;
 function doSomeStuff() {
-  console.log('doSomeStuff is called.....');
-  return;
+    console.log('doSomeStuff is called.....');
+    return;
 }
 exports.doSomeStuff = doSomeStuff;
 doSomeStuff();
