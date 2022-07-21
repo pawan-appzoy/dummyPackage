f75399ad32bab28568f5105dac0cc400dc3de26b
pawan-appzoy <pawan@appzoy.com>
2022-07-21 17:19:45 +0530

    env changes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
+import 'dotenv/config';
 import {Employee} from './example/dummyClass';
 
-export let employee = new Employee('Sample message', 100, 'pawan');
+//... Example 1 ....//
+export let employee = new Employee(
+  'Sample message from instance',
+  100,
+  'pawan'
+);
 
+//... Example 2 ....//
 export function sendMessage() {
   let employee = new Employee('Sample message', 100, 'pawan');
   return employee.sendMessage();
 }
 
+//... Example 3 ....//
 export function doSomeStuff() {
-  console.log('doSomeStuff is called.....');
+  console.log('doSomeStuff is called env : ', process.env.ENV);
 
   return;
 }
